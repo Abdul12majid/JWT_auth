32c24f2f7115ed8cef2515df69b48e6d25d4aa5a
Abdul12majid <yisaabdulmajid@gmail.com>
2024-11-15 14:10:56 +0100

    register view/url

--- auth_app/views.py
+++ auth_app/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render, HttpResponse
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from .serializers import RegisterSerializer
 
-# Create your views here.
+
 def index(request):
 	return HttpResponse("Holla")
+	
+
+@api_view(['POST'])
+def register(request):
+    serializer = RegisterSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
