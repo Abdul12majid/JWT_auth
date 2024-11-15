b5cdbdd729cfa32598341628a7ddc5e60dd2b44c
Abdul12majid <yisaabdulmajid@gmail.com>
2024-11-15 14:19:01 +0100

    all_users view/url

--- auth_app/urls.py
+++ auth_app/urls.py
@@ -12,4 +12,5 @@ urlpatterns = [
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),  # For refreshing the token
     path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),     # For verifying the token
     path('api/register/', views.register, name='register'),
+    path('api/users/', views.all_users, name='users'),
 ]
--- auth_app/views.py
+++ auth_app/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render, HttpResponse
 from rest_framework import status
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
 from .serializers import RegisterSerializer
+from rest_framework.permissions import IsAuthenticated
 
 
 def index(request):
 	return HttpResponse("Holla")
-	
+
 
 @api_view(['POST'])
 def register(request):
@@ -16,3 +17,11 @@ def register(request):
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def all_users(request):
+	users = User.objects.all()
+	serializer = RegisterSerializer(users, many=True)
+	return Response(serializer.data, status=status.HTTP_201_CREATED)
