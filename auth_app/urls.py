d20adb38539a158143d1b8dca235a495a9c0c118
Abdul12majid <yisaabdulmajid@gmail.com>
2024-11-15 14:02:41 +0100

    added JWT urls

--- auth_app/urls.py
+++ auth_app/urls.py
@@ -1,8 +1,14 @@
 from . import views
 from django.urls import path
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView,
+)
 
 urlpatterns = [
     path('', views.index, name='index'),
-    
-
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),  # For obtaining a new token
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),  # For refreshing the token
+    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),     # For verifying the token
 ]
